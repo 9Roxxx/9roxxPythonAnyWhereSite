75f695ef15b16077e905d21eaf7b2a66830dc6aa
unknown <akbaralievsarvar4@yandex.com>
2022-12-11 23:45:24 +0300

    Try4

--- videostore/settings.py
+++ videostore/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd0$@lr-*clt^)$tx#$6v5d@(mmt4p&osi5+h(q47ff^*yy$_in'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
